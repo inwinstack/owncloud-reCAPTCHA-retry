1babb91d264ea2b4db6820b8fc0a06ab31c84d13
林韋霖 <blin9533@gmail.com>
2018-06-04 13:13:42 +0800

    Initial commit

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php 
+$OC_Version = array(10,0,8,5);
+$OC_VersionString = '10.0.8';
+$OC_Edition = '';
+$OC_Channel = 'stable';
+$OC_VersionCanBeUpgradedFrom = [[8,2,11],[9,0,9],[9,1]];
+$OC_Build = '2018-04-19T16:17:44+00:00 ef478a9735e552adc22f698172d67acca4e0dc63';
+$vendor = 'owncloud';
